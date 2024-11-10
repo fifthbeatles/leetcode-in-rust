377978f106f599b2ddacb816399f65232a894841
chenliang10 <chenliang10@baidu.com>
2024-11-10 22:15:25 +0800

    409. Longest Palindrome

--- src/problems/string/easy.rs
+++ src/problems/string/easy.rs
@@ -4,3 +4,4 @@ pub mod p00028_find_the_index_of_the_first_occurrence_in_a_string;
 pub mod p00125_valid_palindrome;
 pub mod p00242_valid_anagram;
 pub mod p00383_ransom_note;
+pub mod p00409_longest_palindrome;
