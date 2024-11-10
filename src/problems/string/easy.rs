c936b6a431d208873d0e7a48c1f85e9d78f3d815
chenliang10 <chenliang10@baidu.com>
2024-11-10 22:13:37 +0800

    383. Ransom Note

--- src/problems/string/easy.rs
+++ src/problems/string/easy.rs
@@ -3,3 +3,4 @@ pub mod p00020_valid_parentheses;
 pub mod p00028_find_the_index_of_the_first_occurrence_in_a_string;
 pub mod p00125_valid_palindrome;
 pub mod p00242_valid_anagram;
+pub mod p00383_ransom_note;
